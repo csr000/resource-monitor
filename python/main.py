b411c4d912caea69c0c45c51e5152863b6e5964f
csr000 <csrichard000@gmail.com>
2022-10-05 14:42:50 +0100

    🐛  fix alignment

--- python/main.py
+++ python/main.py
@@ -2,21 +2,13 @@ import PySimpleGUI as sg
 import psutil
 
 
-def scheme(ram="-", cpu="-"):
-    return f"{ram} {cpu}"
-
-
 # build layout
 sg.theme("DarkAmber")
 sg.set_options(element_padding=(0, 0))
 layout = [
     [
-        sg.Text("RAM", justification="center", font=("Helvetica", 8)),
-        sg.Text("CPU", justification="center", font=("Helvetica", 8)),
-    ],
-    [
-        sg.Text("-", justification="center", key="RAM"),
-        sg.Text("-", justification="center", key="CPU"),
+        sg.Column([[sg.Text("CPU", font=("Helvetica", 8))], [sg.Text("-", key="CPU")]]),
+        sg.Column([[sg.Text("RAM", font=("Helvetica", 8))], [sg.Text("-", key="RAM")]]),
     ],
 ]
 
@@ -33,5 +25,5 @@ window = sg.Window(
 
 while True:  # Event Loop
     event, values = window.read(timeout=1000)
-    window["RAM"].update(int(psutil.virtual_memory().percent))
     window["CPU"].update(int(psutil.cpu_percent()))
+    window["RAM"].update(int(psutil.virtual_memory().percent))
